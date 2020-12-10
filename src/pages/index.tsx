48b19b150a94544b71bdff7adf4b2ec0658cc4e4
Diego Buriti <bs.diego97@gmail.com>
2020-12-10 08:45:42 -0300

    add typescript

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+type Props = {
+  title: string
+}
+
+export default function Home({ title }: Props) {
   return (
     <div className={styles.container}>
       <Head>
