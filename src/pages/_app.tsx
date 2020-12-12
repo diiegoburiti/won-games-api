667d8ee89cf3e472c48bcf7018a804cb89d31ac3
Diego Buriti <bs.diego97@gmail.com>
2020-12-12 09:50:48 -0300

    add initial styles with styled-components

---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+import GlobalStyles from 'styles/globals'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>React Avançado Boilerplate</title>
+        <link rel="stylesheet" href="/img/icon-512.png/" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png/" />
+        <meta
+          name="description"
+          content="a simple project started to work with TypeScript, React, Styled Components and NextJS"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
+}
