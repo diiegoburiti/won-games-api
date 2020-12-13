ea6effe77620998f749a29d1fd1f8c0e81369bb6
Diego Buriti <bs.diego97@gmail.com>
2020-12-13 17:22:01 -0300

    add next-pwa configs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,8 +7,9 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>React Avançado Boilerplate</title>
-        <link rel="stylesheet" href="/img/icon-512.png/" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png/" />
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
           content="a simple project started to work with TypeScript, React, Styled Components and NextJS"
