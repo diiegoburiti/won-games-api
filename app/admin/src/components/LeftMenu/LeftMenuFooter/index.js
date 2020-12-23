a2775568a32759ccd4de041d1059ca89831b027a
Diego Buriti <bs.diego97@gmail.com>
2020-12-22 22:18:44 -0300

    change text LeftMenuFooter

---
+++ app/admin/src/components/LeftMenu/LeftMenuFooter/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import Wrapper from "./Wrapper";
+
+function LeftMenuFooter() {
+  return (
+    <Wrapper>
+      <div className="poweredBy">
+        <span>Mantido por </span>
+        <a
+          key="website"
+          href="https://reactavancado.com.br"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          React Avançado
+        </a>
+      </div>
+    </Wrapper>
+  );
+}
+
+export default LeftMenuFooter;
