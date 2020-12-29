b1326f38bfb6a567983cc98f8bdc9fcd6b7b8277
Diego Buriti <bs.diego97@gmail.com>
2020-12-29 08:27:45 -0300

    addfirst fetch Gog api

--- app/api/game/services/game.js
+++ app/api/game/services/game.js
@@ -4,5 +4,15 @@
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/services.html#core-services)
  * to customize this service
  */
+const axios = require("axios"); 
 
-module.exports = {};
+
+module.exports = {
+  populate: async (params) => {
+    const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`
+
+    const { data: { products } } = await axios.get(gogApiUrl)
+
+    console.log(products[0])
+  }
+};
