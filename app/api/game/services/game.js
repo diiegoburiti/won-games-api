06e6e0488466a6b8e0624a720ae8579908625c1a
Diego Buriti <bs.diego97@gmail.com>
2020-12-30 09:27:02 -0300

    add createGames  method

--- app/api/game/services/game.js
+++ app/api/game/services/game.js
@@ -25,7 +25,7 @@ async function getGameInfo(slug) {
           .getAttribute("xlink:href")
           .replace(/_/g, "")
           .replace(/[^\w-]+/g, "")
-      : "Free",
+      : "FREE",
     short_description: description.textContent.trim().slice(0, 160),
     description: description.innerHTML,
   };
@@ -76,6 +76,40 @@ async function createManyToManyData(products) {
   ]);
 }
 
+async function createGames(products) {
+  await Promise.all(
+    products.map(async (product) => {
+      const item = await getByName(product.title, "game");
+
+      if (!item) {
+        console.info(`Creating: ${product.title}...`);
+
+        const game = await strapi.services.game.create({
+          name: product.title,
+          slug: product.slug.replace(/_/g, "-"),
+          price: product.price.amount,
+          release_date: new Date(
+            Number(product.globalReleaseDate) * 1000
+          ).toISOString(),
+          categories: await Promise.all(
+            product.genres.map((name) => getByName(name, "category"))
+          ),
+          platforms: await Promise.all(
+            product.supportedOperatingSystems.map((name) =>
+              getByName(name, "platform")
+            )
+          ),
+          developers: [await getByName(product.developer, "developer")],
+          publisher: await getByName(product.publisher, "publisher"),
+          ...(await getGameInfo(product.slug)),
+        });
+
+        return game;
+      }
+    })
+  );
+}
+
 module.exports = {
   populate: async (params) => {
     const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
@@ -84,7 +118,7 @@ module.exports = {
       data: { products },
     } = await axios.get(gogApiUrl);
 
-    await createManyToManyData([products[1], products[4]])
-
+    await createManyToManyData([products[1], products[4]]);
+    await createGames([products[1], products[4]])
   },
 };
