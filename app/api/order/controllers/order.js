cc6e9ee7d4311751d7f96997d12e35b5199ef165
Diego Buriti <bs.diego97@gmail.com>
2021-06-28 08:35:41 -0300

    chore: user info inside create controller

--- app/api/order/controllers/order.js
+++ app/api/order/controllers/order.js
@@ -56,4 +56,16 @@ module.exports = {
       };
     }
   },
+
+  create: async (ctx) => {
+    const {cart, paymentIntentId, paymentMethod} = ctx.request.body;
+
+    const token = await strapi.puglins["users-permitions"].services.jwt.getToken(ctx);
+
+    const userId = token.id;
+
+    const userInfo = await strapi.query("user", "users-permissions").findOne({id: userId})
+
+    return {cart, paymentIntentId, paymentMethod, userInfo}
+  }
 };
